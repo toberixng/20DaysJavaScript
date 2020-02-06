9d39deee9ee39b3ed780e09ee0832f022e57af4f
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-06 17:19:40 +0100

    folder update

---
+++ movingSeat/script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+
+//Update total and Count
+const updateSelectedCount=() =>{
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+})
+
+//Seat click event
+container.addEventListener('click', (e) => {
+ let classTarget = e.target.classList;
+
+ if(classTarget.contains('seat') && 
+ !classTarget.contains('occupied')) {
+   classTarget.toggle('selected');
+   updateSelectedCount();
+ }
+})
+
