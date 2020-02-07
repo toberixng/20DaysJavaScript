9d68717bf78b0cbab7c481cf186bfad3187acd74
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-07 19:59:39 +0100

    time update

--- video-player/script.js
+++ video-player/script.js
@@ -4,3 +4,9 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+
+// Event Listener
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('click', togglePlayIcon);
+video.addEventListener('click', togglePlayIcon);
+video.addEventListener('timeupdate', toggleVideoStatus);
