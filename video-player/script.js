88144d0ece05434e74955037bf91585908b1ce6e
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-08 09:44:42 +0100

    play icon

--- video-player/script.js
+++ video-player/script.js
@@ -4,9 +4,25 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+  return true;
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+  return true;
+}
 
 // Event Listener
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('click', togglePlayIcon);
 video.addEventListener('click', togglePlayIcon);
-video.addEventListener('timeupdate', toggleVideoStatus);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
+
