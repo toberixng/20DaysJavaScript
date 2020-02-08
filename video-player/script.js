4542708d0a215fc88a2420040566d678189a469d
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-08 10:05:57 +0100

    icon update

--- video-player/script.js
+++ video-player/script.js
@@ -11,7 +11,11 @@ function toggleVideoStatus() {
 
 // update play/pause icon
 function updatePlayIcon() {
-  return true;
+  if(video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>'
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+  } 
 }
 
 // Update progress & timestamp
