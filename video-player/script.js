c419cbaeab81455bb379327506645539d7ea6038
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-09 22:02:39 +0100

    exchange rate folder

--- video-player/script.js
+++ video-player/script.js
@@ -6,39 +6,53 @@ const timestamp = document.getElementById('timestamp');
 
 // Play & pause video
 function toggleVideoStatus() {
-  return (video.paused) ? video.play() : video.pause()
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
-
 
 // update play/pause icon
 function updatePlayIcon() {
-  return (video.paused) ? 
-    play.innerHTML = '<i class="fa fa-play fa-2x"></i>' :
-    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>'
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
-
 
 // Update progress & timestamp
 function updateProgress() {
   progress.value = (video.currentTime / video.duration) * 100;
 
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-
 // Set video time to progress
-function setVideoProgress(){
+function setVideoProgress() {
   video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-
 // Stop video
-function stopVideo(){
+function stopVideo() {
   video.currentTime = 0;
-  video.pause()
+  video.pause();
 }
 
-
-// Event Listener
+// Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
@@ -49,4 +63,3 @@ play.addEventListener('click', toggleVideoStatus);
 stop.addEventListener('click', stopVideo);
 
 progress.addEventListener('change', setVideoProgress);
-
