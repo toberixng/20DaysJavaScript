8152319c8359514a9aaf80dddd7ca858c4721f68
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-10 19:21:17 +0100

    item json file deleted

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -1,9 +1,5 @@
 function calculate() {
-  fetch('items.json')
-    .then(res => res.json())
-    .then(data => data.forEach(element => {
-      document.body.innerHTML = Object.values(element)
-    }));
+  
 }
 
 calculate();
