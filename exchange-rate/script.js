ff4220d66c669d92c94bec81a41beb9df927f117
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-10 19:31:19 +0100

    exchange rates and  the dom update

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -1,6 +1,12 @@
 const currencyEl_one = document.getElementById('currency-one');
 const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
 
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the dom
 function calculate() {
   
 }
