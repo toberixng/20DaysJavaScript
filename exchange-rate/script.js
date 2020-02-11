758b3933404d369611e3aedf860ea7cf4830ae35
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-12 00:18:53 +0100

    SWAP FINAL UPDATE

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -29,5 +29,12 @@ amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
 amountEl_two.addEventListener('input', calculate);
 
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calCulate();
+});
+
 
 calculate();
