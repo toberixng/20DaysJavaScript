d03bc53263551ada2c7dfa2496ff94d8001b4bf7
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-12 00:15:23 +0100

    SWAPBUTTON

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -1,29 +1,29 @@
-'use strict';
-
 const currencyEl_one = document.getElementById('currency-one');
-const currencyEl_two = document.getElementById('currency-two');
 const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
 const amountEl_two = document.getElementById('amount-two');
 
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-// Fetch exchange rates and update the dom
+// Fetch exchange rates and update the DOM
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
     .then(res => res.json())
-    .then(data =>{
-      // console.log(data)
+    .then(data => {
+      // console.log(data);
       const rate = data.rates[currency_two];
-      console.log(rate)
-   });
+
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
 }
 
-
-// Event Listeners
+// Event listeners
 currencyEl_one.addEventListener('change', calculate);
 amountEl_one.addEventListener('input', calculate);
 currencyEl_two.addEventListener('change', calculate);
