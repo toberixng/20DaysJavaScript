c4fb039cc02a0ca9a71e5d001c1cf433ba686787
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-14 18:41:40 +0100

    package json created

--- ArrayMethod/script.js
+++ ArrayMethod/script.js
@@ -4,3 +4,9 @@ const doubleBtn = document.getElementById('double');
 const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+
+// Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me.app')
+}
