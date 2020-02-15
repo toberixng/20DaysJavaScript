8ffcd43994a7d4d55a213a161e971bf64c288cbe
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-15 18:08:33 +0000

    create an element and add class

--- ArrayMethod/script.js
+++ ArrayMethod/script.js
@@ -28,5 +28,18 @@ async function getRandomUser() {
 
 // Add new obj to data arr
 function addData (obj) {
-    data.push(obj)
+    data.push(obj);
+
+    updateDOM();
+}
+
+// Update DOM
+function updateDom(providedData = data) {
+    // clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+    });
 }
