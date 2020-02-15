c1345ff1664936b9a7ac7603b4f79f3da815e83d
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-15 14:05:33 +0000

    new users update

--- ArrayMethod/script.js
+++ ArrayMethod/script.js
@@ -5,8 +5,22 @@ const showMillionairesBtn = document.getElementById('show-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
 
 // Fetch random user and add money
 async function getRandomUser() {
-    const res = await fetch('https://randomuser.me.app')
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    console.log(data)
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+    console.log(newUser)
 }
