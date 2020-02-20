8b222080e98d59d68cfd77c9608cf8546063792f
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-20 19:53:21 +0100

    script toggle update

---
+++ MenuSlider/script.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+// Toggle Navigation
+
+toggle.addEventListener('click', () => 
+document.body.classList.toggle('show-nav')
+);
