4af2c6e2802b97078923711d2083324dc01a1dee
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-24 17:57:08 +0100

    fetch data

--- mealFinder/scripts.js
+++ mealFinder/scripts.js
@@ -6,7 +6,21 @@ const search = document.getElementById('search'),
   single_mealEl = document.getElementById('single-meal');
 
 
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
 
+  // clear single meal
+  single_mealEl.innerHTML = '';
+
+  // Get the search term
+  const term = search.value;
+  if(term.trim()){
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+}
 
    //Even Listeners
    submit.addEventListener('submit', searchMeal)
