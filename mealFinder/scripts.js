fbc87cce3768e078c18299c8a97debc576cda03e
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-02-24 17:38:23 +0100

    dome variable created

---
+++ mealFinder/scripts.js
@@ -0,0 +1,6 @@
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
