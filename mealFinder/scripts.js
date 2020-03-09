58d61d3ea1c113ec8befacf7147d007104768261
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-03-09 18:53:12 +0100

    total refactoring

--- mealFinder/scripts.js
+++ mealFinder/scripts.js
@@ -47,7 +47,6 @@ function searchMeal(e) {
   }
 }
 
-<<<<<<< HEAD
 // Fetch meal by ID
 function getMealById(mealID) {
   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
@@ -59,7 +58,20 @@ function getMealById(mealID) {
     });
 }
 
+// Fetch random meal from API
+function getRandomMeal() {
+  // Clear meals and heading
+  mealsEl.innerHTML = '';
+  resultHeading.innerHTML = '';
 
+  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
 
 // Add meal to DOM
 function addMealToDOM(meal) {
@@ -96,7 +108,7 @@ function addMealToDOM(meal) {
 
 // Event listeners
 submit.addEventListener('submit', searchMeal);
-// random.addEventListener('click', getRandomMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
   const mealInfo = e.path.find(item => {
@@ -112,50 +124,3 @@ mealsEl.addEventListener('click', e => {
     getMealById(mealID);
   }
 });
-=======
-// Fetch random meal from API
-function getRandomMeal() {
-  // Clear meals and heading
-  mealsEl.innerHTML = '';
-  resultHeading.innerHTML = '';
-
-  fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
-    .then(res => res.json())
-    .then(data => {
-      const meal = data.meals[0];
-
-      addMealToDOM(meal);
-    });
-}
-
-function addMealToDOM(meal) {
-  const ingredients = [];
-
-  for (let i = 1; i <= 20; i++) {
-    if (meal[`strIngredient${i}`]) {
-      ingredients.push(
-        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
-      );
-    } else {
-      break;
-    }
-  }
-  
-   //Even Listeners
-   submit.addEventListener('submit', searchMeal);
-
-   mealsEl.addEventListener('click', e => {
-    const mealInfo = e.path.find(item => {
-      if (item.classList) {
-        return item.classList.contains('meal-info');
-      } else {
-        return false;
-      }
-    });
-  
-    if (mealInfo) {
-      const mealID = mealInfo.getAttribute('data-mealid');
-      getMealById(mealID);
-    }
-  });
->>>>>>> 935469bbe0cad1a331f4b4261b1db9651ca9f10b
