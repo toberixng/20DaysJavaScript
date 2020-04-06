f1964dc62c9b0f2c5cf2df919b2b0a432e734cb4
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-04-06 20:35:21 +0100

    LOCALSTORAGE2

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -7,14 +7,16 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 
-const dummyTransactions = [
-  { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-];
+// const dummyTransactions = [
+//   { id: 1, text: 'Flower', amount: -20 },
+//   { id: 2, text: 'Salary', amount: 300 },
+//   { id: 3, text: 'Book', amount: -10 },
+//   { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 
 // Add Transaction manually
@@ -35,6 +37,8 @@ function addTransaction(e){
     addTransactionDOM(transaction);
 
     updateValues()
+
+    updateLocalStorage()
 
     text.value = '';
     amount.value = '';    
@@ -94,9 +98,13 @@ function updateValues() {
 function removeTransaction(id) {
   transactions = transactions.filter(transaction => transaction.id !== id);
 
+  updateLocalStorage()
   init();
 }
 
+function updateLocalStorage(){
+  localStorage.setItem('transactions', JSON.stringify(transactions))
+}
 
 // Init app
 function init() {
